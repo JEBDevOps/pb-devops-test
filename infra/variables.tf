dd198f35b79b6ebc7d8e4c998df5e3257f280969
Justin Evan Belga <justinevanbelga@Justins-MacBook-Air.local>
2025-09-04 19:07:11 +0800

    feat: adding the basic variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,28 @@
+variable "project_name" {
+  type        = string
+  default     = "pb-test"
+  description = "The name of the project."
+}
 
+variable "github_org" {
+  type        = string
+  default     = "JEBDevOps"
+  description = "The GitHub organization where the repository is located."
+}
+
+variable "github_repo" {
+  type        = string
+  default     = "pb-devops-test"
+  description = "The name of the GitHub repository."
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "The AWS account ID."
+}
+
+variable "region" {
+  type        = string
+  default     = "ap-southeast-1"
+  description = "The AWS region to deploy the resources in."
+}
