e2c18d062830ecb06ae7c07424e78146c3c392f8
Justin Evan Belga <justinevanbelga@Justins-MacBook-Air.local>
2025-09-04 21:48:43 +0800

    feat: adding api gateway resources

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -41,3 +41,82 @@ resource "aws_lambda_function" "get_item_lambda" {
     }
   }
 }
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "${var.project_name}-api"
+  description = "API for the ${var.project_name} project"
+}
+
+resource "aws_api_gateway_resource" "items" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "items"
+}
+
+resource "aws_api_gateway_resource" "item" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.items.id
+  path_part   = "{id}"
+}
+
+resource "aws_api_gateway_method" "post_items" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.items.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "get_item" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.item.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "post_items_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.items.id
+  http_method = aws_api_gateway_method.post_items.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.create_item_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "get_item_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.item.id
+  http_method = aws_api_gateway_method.get_item.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.get_item_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.post_items_integration,
+    aws_api_gateway_integration.get_item_integration,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  #stage_name  = "prod"
+}
+
+resource "aws_lambda_permission" "api_gateway_permission_post" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.create_item_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "api_gateway_permission_get" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.get_item_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,5 @@
 
+output "api_url" {
+  description = "The invoke URL for the API Gateway stage"
+  value       = aws_api_gateway_deployment.api_deployment.invoke_url
+}
