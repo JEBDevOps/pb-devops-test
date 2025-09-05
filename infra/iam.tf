af86181ea300b98d65e9689c96796b7904719beb
JEBDev <belga.justin.evan@gmail.com>
2025-09-05 12:04:04 +0800

    refactor: fixing the naming so they're clearer and removing unnecessary permissions

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_iam_role" "lambda_role" {
-  name = "${var.project_name}-lambda-role"
+  name = "${var.project_name}-lambda-execution-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -17,30 +17,37 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "aws_iam_policy" "dynamodb_policy" {
-  name        = "${var.project_name}-dynamodb-policy"
-  description = "Policy for Lambda to access the DynamoDB table"
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "${var.project_name}-lambda-policy"
+  description = "IAM policy for Lambda to access DynamoDB and CloudWatch Logs"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
+        Effect = "Allow"
         Action = [
-          "dynamodb:Query",
-          "dynamodb:Scan",
           "dynamodb:GetItem",
           "dynamodb:PutItem",
           "dynamodb:UpdateItem",
           "dynamodb:DeleteItem"
         ]
-        Effect   = "Allow"
-        Resource = "arn:aws:dynamodb:${var.region}:${data.aws_caller_identity.current.account_id}:table/${aws_dynamodb_table.pb_test_table.name}"
+        Resource = aws_dynamodb_table.pb_test_table.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.project_name}-*:*"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "dynamodb_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.dynamodb_policy.arn
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
