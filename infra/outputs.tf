55d9a107c7247e1745742593038937fcfbece94f
Justin Evan Belga <justinevanbelga@Justins-MacBook-Air.local>
2025-09-05 08:06:45 +0800

    feat: adding output for api gateway url

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,5 @@
 
 output "api_url" {
   description = "The invoke URL for the API Gateway stage"
-  value       = aws_api_gateway_deployment.api_deployment.invoke_url
+  value       = aws_api_gateway_stage.api_stage.invoke_url
 }
